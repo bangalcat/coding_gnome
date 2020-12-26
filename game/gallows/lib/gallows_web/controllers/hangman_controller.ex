0683e4047f500298627393715be50b05d86ed121
sunriseslowy <semaj9310@gmail.com>
2020-12-26 14:42:44 +0900

    gallows: make hangman game page. add graphics

--- game/gallows/lib/gallows_web/controllers/hangman_controller.ex
+++ game/gallows/lib/gallows_web/controllers/hangman_controller.ex
@@ -12,4 +12,12 @@ defmodule GallowsWeb.HangmanController do
     |> put_session(:game, game)
     |> render("game_field.html", tally: Hangman.tally(game))
   end
+
+  def make_move(conn, params) do
+    guess = params["make_move"]["guess"]
+    tally = get_session(conn, :game) |> Hangman.make_move(guess)
+
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render("game_field.html", tally: tally)
+  end
 end
